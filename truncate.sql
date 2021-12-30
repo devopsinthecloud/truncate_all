61bf02a1596620bb9ba36134d8d0fd760947706c
Marie <49714157+devopsinthecloud@users.noreply.github.com>
2021-12-30 14:09:44 +0200

    Create truncate.sql

---
+++ truncate.sql
@@ -0,0 +1,11 @@
+DO
+$func$
+BEGIN
+   EXECUTE
+   (SELECT 'TRUNCATE TABLE ' || string_agg(oid::regclass::text, ', ') || ' CASCADE'
+    FROM   pg_class
+    WHERE  relkind = 'r'
+    AND    relnamespace = 'sch'::regnamespace
+   );
+END
+$func$;
